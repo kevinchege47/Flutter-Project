71f9cab2f8cd293ea24e13abb8180755d24e3e90
kevinchege47 <chegekevin001@gmail.com>
2022-11-16 10:28:21 +0300

    Added container properties

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -10,18 +10,36 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Dashboard'.toUpperCase()),
-        backgroundColor: Colors.lightGreen,
-      ),
-      body: Center(
-        child: IconButton(
-          icon: Icon(FontAwesomeIcons.airbnb),
-          iconSize: 100,
-          color: Colors.lightGreen,
-          splashColor: Colors.deepOrange,
-          onPressed: () {},),
+      // appBar: AppBar(
+      //   title: Text('Dashboard'.toUpperCase()),
+      //   backgroundColor: Colors.lightGreen,
+      // ),
+      body: Container(
+        width: 350.0,
+        height: 250.0,
+        padding: EdgeInsets.all(20.0),
+        margin: EdgeInsets.all(50.0),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.deepOrange,
+          // borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(color: Colors.black,width: 6.0),
+          shape: BoxShape.circle,
+          image: DecorationImage(image: AssetImage("images/rocket.jpg")),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.amber,
+              blurRadius: 7,
+              spreadRadius: 5,
+              offset: Offset(4,4)
+            )
+
+          ],
+        ),
+        // child: Text(
+        //   "Little less Boring",
+        //   style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        // ),
       ),
     );
   }
