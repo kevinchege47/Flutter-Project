9040332db8c5ebcc4ba8079cbb4efdc2bf11932e
kevinchege47 <chegekevin001@gmail.com>
2022-11-21 10:22:09 +0300

    Rows,Columns,Expanded widget

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 // ignore_for_file: prefer_const_constructors
@@ -7,42 +9,52 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        leading: IconButton(icon:Icon(Icons.menu),onPressed: (){}),
-        title: Text("Home"),
-        actions: [
-          IconButton(icon:Icon(Icons.shopping_cart),onPressed: (){}),
-          IconButton(icon:Icon(Icons.search),onPressed: (){}),
-          // IconButton(icon:Icon(Icons.help),onPressed: (){}),
-        ],
-        elevation: 5,
+        title: Text("Rows and Columns"),
         centerTitle: true,
-        backgroundColor: Colors.yellow,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
-        ) ,
       ),
-      body: Center(
-        child: Directionality(
-          textDirection: TextDirection.rtl,
-          child: ElevatedButton.icon(
-            icon: Icon(Icons.add_a_photo_outlined),
-            label: Text("Take a Photo"),
-            // child: Text("Lets Begin"),
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.black,
-              padding: EdgeInsets.all(20.0),
-              // backgroundColor: Colors.deepOrange,
-              fixedSize: Size(300, 80),
-              textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              // elevation: 10,
-              shadowColor: Colors.black,
-              // side: BorderSide(color: Colors.black, width: 1),
-              shape: StadiumBorder(),
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(child: Image(image: AssetImage("images/rocket.jpg"))),
+                Expanded(flex:2, child: Image(image: AssetImage("images/rocket.jpg"))),
+                Expanded(child: Image(image: AssetImage("images/rocket.jpg"))),
+                Expanded(flex:4,child: Image(image: AssetImage("images/rocket.jpg"))),
+              ],
             ),
-          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star),
+                Icon(Icons.star),
+                Icon(Icons.star_border_outlined),
+                Icon(Icons.star_border_outlined),
+                Icon(Icons.star_border_outlined),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [Icon(Icons.phone, size: 35.0), Text("Phone")],
+                ),
+                Column(
+                  children: [Icon(Icons.home, size: 35.0), Text("Home")],
+                ),
+                Column(
+                  children: [Icon(Icons.share, size: 35.0), Text("Share")],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.add_card_rounded, size: 35.0),
+                    Text("Card")
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
